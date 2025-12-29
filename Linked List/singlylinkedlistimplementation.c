f2b77a54379badf101ee25877e5dab1649e26959
sassy-86 <sashvin0.8.0@gmail.com>
2025-12-29 16:05:44 +0530

    singly linked list implementation in C
    Implement a singly linked list with append and display functions.

---
+++ Linked List/singlylinkedlistimplementation.c
@@ -0,0 +1,62 @@
+//singly linked list creation
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+    int data;
+    struct node *next;
+    
+};
+
+struct node *head =NULL;
+
+void append(int val)
+{
+    struct node *newnode = malloc(sizeof(struct node));
+    newnode->data = val;
+    newnode->next = NULL;
+    if(head == NULL)
+    {
+        
+        head = newnode;
+    }
+    else{
+        struct node *temp=head;
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        
+    }
+}
+
+void display(){
+    struct node *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+
+
+int main(){
+    int n, val;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&val);
+        if(val>0){
+            append(val);
+        }
+    
+    }
+
+    display();
+   
+    return 0;
+}
+}
